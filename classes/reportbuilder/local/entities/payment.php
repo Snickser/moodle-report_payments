7bd525bd6f44eb9af11977c17202c94fc8571410
root <snickser@gmail.com>
2024-05-15 09:55:01 +0300

    add

--- classes/reportbuilder/local/entities/payment.php
+++ classes/reportbuilder/local/entities/payment.php
@@ -133,6 +133,7 @@ class payment extends base {
             ->set_type(column::TYPE_TEXT)
             ->add_field("{$tablealias}.amount")
             ->set_is_sortable(true)
+            ->add_attributes(['class' => 'text-right'])
             ->add_callback(function (?string $value): string {
                 return ($value === '') ? '0' : number_format(floatval($value), 2);
             });
